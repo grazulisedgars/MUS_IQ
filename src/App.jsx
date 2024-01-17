f3607babfb98b9e6ff9ba39d0502361045822a46
grazulisedgars <grazulisedgars@gmail.com>
2024-01-17 15:36:11 +0200

    Adds QuestionPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import WelcomePage from './components/WelcomePage';
+import QuestionPage from './components/QuestionPage';
 
 function App() {
-  return <WelcomePage />;
+  return (
+    <>
+      <WelcomePage />
+      <QuestionPage />
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/QuestionPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function QuestionPage() {
+    return (
+        <>
+            <div>
+                <h3>Question:</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odit, sed natus eligendi nihil nobis excepturi hic cumque, provident atque est. Dolore, quia? Quod nesciunt dignissimos nulla quisquam voluptatem eum!</p>
+            </div>
+            <div>
+                <button>Answer 1</button>
+                <button>Answer 2</button>
+                <button>Answer 3</button>
+                <button>Answer 4</button>
+            </div>
+            <p>1 of 10</p>
+        </>
+    )
+}
+
+export default QuestionPage;
